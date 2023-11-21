0d57ee6b3ce61a89f511f90987d0900c48fe7478
Moxxy <mikkelinis@gmail.com>
2023-11-21 09:18:07 +0100

    Added PotionController and WeaponController

---
+++ TextRPG.API/Controllers/PotionController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TextRPG.Repository.Interfaces;
+using TextRPG.Repository.Models;
+using TextRPG.Repository.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TextRPG.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PotionController : ControllerBase
+    {
+        IBaseCRUDRepo<Potion> PotionRepo { get; set; }
+        public PotionController(IBaseCRUDRepo<Potion> potionRepo)
+        {
+            PotionRepo = potionRepo;
+        }
+
+        // GET: api/<PotionController>
+        [HttpGet]
+        public List<Potion> Get()
+        {
+            return PotionRepo.GetAll();
+        }
+
+        // GET api/<PotionController>/5
+        [HttpGet("{id}")]
+        public Potion Get(int id)
+        {
+            return PotionRepo.GetById(id);
+        }
+
+        // POST api/<PotionController>
+        [HttpPost]
+        public void Post([FromBody] Potion potion)
+        {
+            PotionRepo.Create(potion);
+        }
+
+        // PUT api/<PotionController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Potion newPotion)
+        {
+            var oldPotion = PotionRepo.GetById(newPotion.Id);
+
+            if (newPotion == oldPotion) Console.Write("Potion already exists");
+
+            else
+            {
+                oldPotion.Amount = newPotion.Amount;
+                oldPotion.PotionType = newPotion.PotionType;
+
+                PotionRepo.Update(oldPotion);
+            }
+        }
+
+        // DELETE api/<PotionController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            PotionRepo.Delete(id);
+        }
+    }
+}
---
+++ TextRPG.API/Controllers/WeaponController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TextRPG.Repository.Interfaces;
+using TextRPG.Repository.Models;
+using TextRPG.Repository.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TextRPG.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeaponController : ControllerBase
+    {
+        IBaseCRUDRepo<Weapon> WeaponRepo { get; set; }
+        public WeaponController(IBaseCRUDRepo<Weapon> weaponRepo)
+        {
+            WeaponRepo = weaponRepo;
+        }
+
+        // GET: api/<WeaponController>
+        [HttpGet]
+        public List<Weapon> Get()
+        {
+            return WeaponRepo.GetAll();
+        }
+
+        // GET api/<WeaponController>/5
+        [HttpGet("{id}")]
+        public Weapon Get(int id)
+        {
+            return WeaponRepo.GetById(id);
+        }
+
+        // POST api/<WeaponController>
+        [HttpPost]
+        public void Post([FromBody] Weapon weapon)
+        {
+            WeaponRepo.Create(weapon);
+        }
+
+        // PUT api/<WeaponController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Weapon newWeapon)
+        {
+            var oldWeapon = WeaponRepo.GetById(newWeapon.Id);
+
+            if (newWeapon == oldWeapon) Console.Write("Weapon already exists");
+
+            else
+            {
+                oldWeapon.WeaponDamageModifier = newWeapon.WeaponDamageModifier;
+                oldWeapon.MinimumSkillRoll = newWeapon.MinimumSkillRoll;
+                oldWeapon.Range = newWeapon.Range;
+                oldWeapon.AvailableToHero = newWeapon.AvailableToHero;
+                oldWeapon.StarterWeapon = newWeapon.StarterWeapon;
+                oldWeapon.Value = newWeapon.Value;
+                oldWeapon.Note = newWeapon.Note;
+                oldWeapon.WeaponType = newWeapon.WeaponType;
+
+                WeaponRepo.Update(oldWeapon);
+            }
+        }
+
+        // DELETE api/<WeaponController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            WeaponRepo.Delete(id);
+        }
+    }
+}

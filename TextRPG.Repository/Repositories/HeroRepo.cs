b447758a21055baf42bf8257cccf853904d39c3c
Ditjo <mads.ditjo@gmail.com>
2023-11-28 08:53:31 +0100

    -HeroRepo Update Finished?

--- TextRPG.Repository/Repositories/HeroRepo.cs
+++ TextRPG.Repository/Repositories/HeroRepo.cs
@@ -68,7 +68,7 @@ namespace TextRPG.Repository.Repositories
                 .ThenInclude(x => x!.SkillRollType)
                 .Include(x => x.Race)
                 .Include(x => x.Career)
-                .FirstAsync();
+                .FirstAsync(x => x.Id == id);
         }
         /*
         public Hero GetById(int id)
@@ -104,7 +104,7 @@ namespace TextRPG.Repository.Repositories
                 {
                     var weaponsSelected = context.Weapon.
                         Where(w => model.Inventory.Weapons.Select(x => x.Id).ToArray().Contains(w.Id)).ToList();
-                    
+
                     temp.Weapons.AddRange(weaponsSelected);
                 }
                 /*
@@ -134,56 +134,80 @@ namespace TextRPG.Repository.Repositories
         public async Task<Hero?> Update(Hero updateHero)
         {
             Hero hero = await GetById(updateHero.Id);
-            //if (hero != null && updateHero != null)
-            //{
-            //    hero.HeroName = updateHero.HeroName;
-            //    hero.HeroXp = updateHero.HeroXp;
-            //    hero.Level = updateHero.Level;
-            //    hero.Note = updateHero.Note;
-            //    await context.SaveChangesAsync();
-            //}
-            if (updateHero.Inventory != null && hero.Inventory != null)
+
+            if (hero != null && updateHero != null)
             {
-                hero.Inventory.Id = updateHero.Inventory.Id;
-                hero.Inventory.Gold = updateHero.Inventory.Gold;
-                hero.Inventory.ArmourId = updateHero.Inventory.ArmourId;
+                if (!string.IsNullOrWhiteSpace(updateHero.HeroName))
+                    hero.HeroName = updateHero.HeroName;
+                hero.HeroXp = updateHero.HeroXp;
+                hero.Level = updateHero.Level;
+                if (!string.IsNullOrWhiteSpace(updateHero.Note))
+                    hero.Note = updateHero.Note;
 
-                
-                if (updateHero.Inventory.Weapons != null)
+                if (hero.EntityBaseSystem != null && updateHero.EntityBaseSystem != null)
                 {
-                    var weaponsSelected = context.Weapon.
-                        Where(w => updateHero.Inventory.Weapons.Select(x => x.Id).ToArray().Contains(w.Id)).ToList();
-
-                    hero.Inventory.Weapons = weaponsSelected;
+                    hero.EntityBaseSystem.Stength = updateHero.EntityBaseSystem.Stength;
+                    hero.EntityBaseSystem.Agility = updateHero.EntityBaseSystem.Agility;
+                    hero.EntityBaseSystem.Vigor = updateHero.EntityBaseSystem.Vigor;
+                    hero.EntityBaseSystem.Spirit = updateHero.EntityBaseSystem.Spirit;
+                    hero.EntityBaseSystem.Health = updateHero.EntityBaseSystem.Health;
+                    hero.EntityBaseSystem.Energy = updateHero.EntityBaseSystem.Energy;
+                    hero.EntityBaseSystem.HealthModifier = updateHero.EntityBaseSystem.HealthModifier;
+                    hero.EntityBaseSystem.EnergyModifier = updateHero.EntityBaseSystem.EnergyModifier;
+                    hero.EntityBaseSystem.DamagerModifier = updateHero.EntityBaseSystem.DamagerModifier;
+                    hero.EntityBaseSystem.ArmourModifier = updateHero.EntityBaseSystem.ArmourModifier;
                 }
 
-                if (updateHero.Inventory.Potions != null)
+                if (updateHero.Inventory != null && hero.Inventory != null)
                 {
-                    var PotionsSelected = context.Potion.
-                        Where(p => updateHero.Inventory.Potions.Select(x => x.Id).ToArray().Contains(p.Id)).ToList();
+                    hero.Inventory.Id = updateHero.Inventory.Id;
+                    hero.Inventory.Gold = updateHero.Inventory.Gold;
+                    hero.Inventory.ArmourId = updateHero.Inventory.ArmourId;
 
-                    foreach (var potion in updateHero.Inventory.Potions)
+
+                    if (updateHero.Inventory.Weapons != null)
                     {
-                        //if (PotionsSelected.Where(p => )
-                        //{
-                        //    PotionsSelected.Add(potion);
-                        //}
-                    }
-                    hero.Inventory.Potions = PotionsSelected;
-                    //temp.Potions!.AddRange(PotionsSelected);
-                }
-                //updateHero.Inventory = temp;
-            }
-        /*
-        public void Create(Hero model)
-        {
-            context.Hero.Add(model);
-            context.SaveChanges();
-        }*/
+                        var weaponsSelected = context.Weapon.
+                            Where(w => updateHero.Inventory.Weapons.Select(x => x.Id).ToArray().Contains(w.Id)).ToList();
 
-            context.Hero.Update(hero);
-            await context.SaveChangesAsync();
-            return hero;
+                        hero.Inventory.Weapons = weaponsSelected;
+                    }
+
+                    if (updateHero.Inventory.Potions != null && hero.Inventory.Potions != null)
+                    {
+                        List<Potion> temp = new List<Potion>();
+                        foreach (var potion in updateHero.Inventory.Potions)
+                        {
+                            if (hero.Inventory.Potions.Exists(x => x.PotionTypeId == potion.PotionTypeId))
+                            {
+                                //Update potion
+                                var p = hero.Inventory.Potions.Find(x => x.PotionTypeId == potion.PotionTypeId);
+                                if (p != null)
+                                {
+                                    p.InventoryId = potion.InventoryId;
+                                    p.PotionTypeId = potion.PotionTypeId;
+                                    p.Amount = potion.Amount;
+                                }
+                            }
+                            else
+                            {
+                                //Add New potion
+                                temp.Add(new Potion
+                                {
+                                    InventoryId = hero.Inventory.Id,
+                                    PotionTypeId = potion.PotionTypeId,
+                                    Amount = potion.Amount
+                                });
+                            }
+                        }
+                        hero.Inventory.Potions.AddRange(temp);
+                    }
+                }
+                context.Hero.Update(hero);
+                await context.SaveChangesAsync();
+                return hero;
+            }
+            return null;
         }
         /*
         public void Update(Hero model)

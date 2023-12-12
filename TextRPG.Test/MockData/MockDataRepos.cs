6c5c218654a37bd0b20c8d0329b1826a464222c3
Ditjo <mads.ditjo@gmail.com>
2023-12-12 12:32:35 +0100

    -Added MockData & RaceRepoTest.

---
+++ TextRPG.Test/MockData/MockDataRepos.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextRPG.Repository.Models;
+
+namespace TextRPG.Test.MockData
+{
+    internal class MockDataRepos
+    {
+        public static Race GetRaceData(int id)
+        {
+            Race race = new Race()
+            {
+                Id = id,
+                RaceType = $"Race-{id}"
+            };
+            return race;
+        }
+
+        public static Career GetCareerData(int id)
+        {
+            Career career = new Career()
+            {
+                Id = id,
+                CareerType = $"Career-{id}"
+            };
+            return career;
+        }
+
+        public static Armour GetArmourData(int id)
+        {
+            Armour armour = new Armour()
+            {
+                Id = id,
+                ArmourTypeName = $"ArmourType-{id}",
+                ArmourModifier = id,
+                AvailableToHero = true,
+                Value = id,
+                Note = $"Note-{id}"
+            };
+            return armour;
+        }
+
+        public static SkillRollType GetSkillRollTypeData(int id)
+        {
+            SkillRollType skillRollType = new SkillRollType()
+            {
+                Id = id,
+                SkillType = $"SkillType-{id}"
+            };
+            return skillRollType;
+        }
+
+        public static WeaponType GetWeaponTypeData(int id)
+        {
+            WeaponType weaponType = new WeaponType()
+            {
+                Id = id,
+                WeaponTypeName = $"WeaponType-{id}",
+                SkillRollTypeId = id,
+                EnergyCost = id,
+                DamageDice = id,
+                Range = id,
+                SkillRollType = GetSkillRollTypeData(id)
+            };
+            return weaponType;
+        }
+
+        public static Weapon GetWeaponData(int id)
+        {
+            Weapon weapon = new Weapon()
+            {
+                Id = id,
+                WeaponTypeId = id,
+                WeaponName = $"Weapon-{id}",
+                WeaponDamageModifier = id,
+                SkillRoll = id,
+                AvailableToHero = true,
+                StarterWeapon = true,
+                Value = id,
+                Note = $"Note-{id}",
+                WeaponType = GetWeaponTypeData(id)
+                
+            };
+            return weapon;
+        }
+
+        public static PotionType GetPotionTypeData(int id)
+        {
+            PotionType potionType = new PotionType()
+            {
+                Id = id,
+                PotionTypeName = $"PotionTypeName-{id}",
+                PotionDice = id,
+                AvailableToHero = true,
+                Value = id,
+                Note = $"Note-{id}"
+            };
+            return potionType;
+        }
+
+        public static Inventory GetInventoryData(int id)
+        {
+            Inventory inventory = new Inventory()
+            {
+                Id = id,
+                Gold = id
+            };
+            return inventory;
+        }
+
+
+        public static Potion GetpotionData(int id)
+        {
+            Potion potion = new Potion()
+            {
+                Id = id,
+                InventoryId = id,
+                PotionTypeId = id,
+                PotionType = GetPotionTypeData(id)
+            };
+            return potion;
+        }
+
+        public static EntityBaseSystem GetEntityBaseSystemData(int id)
+        {
+            EntityBaseSystem entityBaseSystem = new EntityBaseSystem()
+            {
+                Id = id,
+                Strength = id,
+                Agility = id,
+                Vigor = id,
+                Spirit = id,
+                Health = id,
+                Energy = id,
+                HealthModifier = id,
+                EnergyModifier = id,
+                DamageModifier = id,
+                ArmourModifier = id
+            };
+            return entityBaseSystem;
+        }
+
+        public static Monster GetMonsterData(int id)
+        {
+            Monster monster = new Monster()
+            {
+                Id = id,
+                EntityBaseSystem = GetEntityBaseSystemData(id),
+                Inventory = GetInventoryData(id),
+                MonsterName = $"MonsterName-{id}",
+                MonsterXp = id,
+                LevelDifficulty = id,
+                Note = $"Note-{id}"
+            };
+            return monster;
+        }
+
+        public static Hero GetHeroData(int id)
+        {
+            Hero hero = new Hero()
+            {
+                Id = id,
+                EntityBaseSystem = GetEntityBaseSystemData(id),
+                Inventory = GetInventoryData(id),
+                HeroName = $"HeroName-{id}",
+                HeroXp = id,
+                Level = id,
+                RaceId = id,
+                Race = GetRaceData(id),
+                CareerId = id,
+                Career = GetCareerData(id),
+                Note = $"Note-{id}"
+            };
+            return hero;
+        }
+    }
+}
---
+++ TextRPG.Test/RepositoriesTest/RaceRepoTests.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextRPG.Repository.Models;
+using TextRPG.Repository.Repositories;
+using TextRPG.Repository.Server;
+using TextRPG.Test.MockData;
+
+namespace TextRPG.Test.RepositoriesTest
+{
+    public class RaceRepoTests
+    {
+        //Set up Mock DataBase
+        public Dbcontext context {  get; set; }
+        public DbContextOptions<Dbcontext> options { get; set; }
+
+        private readonly RaceRepo raceRepo;
+        public RaceRepoTests() 
+        {
+            options = new DbContextOptionsBuilder<Dbcontext>()
+                .UseInMemoryDatabase("TestDay").Options;
+
+            context = new Dbcontext(options);
+            raceRepo = new RaceRepo(context);
+        }
+
+        //tests begins here
+
+        [Fact]
+        public async void RaceRepo_CreateNewRace_OnSucces()
+        {
+
+            //Arrange
+            context.Database.EnsureDeleted();
+            context.Add(MockDataRepos.GetRaceData(1));
+            context.Add(MockDataRepos.GetRaceData(2));
+            context.SaveChanges();
+
+            int newRaceId = 3;
+            string newRaceType = "Race-3";
+
+            //Act
+            Race race = new Race()
+            {
+                Id = newRaceId,
+                RaceType = newRaceType,
+            };
+            var returnValue = await raceRepo.Create(race);
+            context.SaveChanges();
+
+            //Assert
+            Assert.Equal(newRaceId, returnValue.Id);
+            Assert.Equal(newRaceType, returnValue.RaceType);
+        }
+
+        //[Fact]
+        //public async void RaceRepo_CreateNewRace_OnFailure()
+        //{
+        //    //Arrange
+        //    context.Database.EnsureDeleted();
+        //    context.Add(MockData.MockDataRepos.GetRaceData(1));
+        //    context.Add(MockData.MockDataRepos.GetRaceData(2));
+        //    context.SaveChanges();
+
+
+        //    //Act
+        //    Race race = new Race();
+        //    var returnValue = await raceRepo.Create(race);
+        //    context.SaveChanges();
+
+        //    //Assert
+        //    //Assert.Fail
+
+        //}
+
+        [Fact]
+        public async void RaceRepo_GetAllRaces_OnSucces()
+        {
+            //Arrange
+            context.Database.EnsureDeleted();
+            context.Add(MockDataRepos.GetRaceData(1));
+            context.Add(MockDataRepos.GetRaceData(2));
+            context.SaveChanges();
+
+            //Act
+            var result = await raceRepo.GetAll();
+            int amount = result.Count();
+
+            //Assert
+            Assert.IsType<List<Race>>(result);
+            Assert.Equal(2, amount);
+
+        }
+
+        [Fact]
+        public async void RaceRepo_GetOneRaceById_OnSucces()
+        {
+            //Arrange
+            context.Database.EnsureDeleted();
+            context.Add(MockDataRepos.GetRaceData(1));
+            context.Add(MockDataRepos.GetRaceData(2));
+            context.SaveChanges();
+            int id = 1;
+
+            //Act
+            var result1 = await raceRepo.GetById(id);
+
+            //Assert
+            Assert.Equal(id, result1.Id);
+        }
+
+        [Fact]
+        public async void RaceRepo_DeleteOneRace_OnSucces()
+        {
+            //Arrange
+            context.Database.EnsureDeleted();
+            context.Add(MockDataRepos.GetRaceData(1));
+            context.Add(MockDataRepos.GetRaceData(2));
+            context.SaveChanges();
+
+            int id = 1;
+
+            //Act
+            var resultbefore = await raceRepo.GetAll();
+            var amountBefore = resultbefore.Count();
+            await raceRepo.Delete(id);
+            var resultAfter = await raceRepo.GetAll();
+            var amountAfter = resultAfter.Count();
+
+            //Assert
+            Assert.NotEqual(amountBefore, amountAfter);
+        }
+
+        [Fact]
+        public async void RaceRepo_UpdateOneRace_OnSucces()
+        {
+            //Arrange
+            context.Database.EnsureDeleted();
+            context.Add(MockDataRepos.GetRaceData(1));
+            context.Add(MockDataRepos.GetRaceData(2));
+            context.SaveChanges();
+
+            int RaceId = 2;
+            string RaceType = "Race2";
+
+            Race race = new Race()
+            {
+                Id = RaceId,
+                RaceType = RaceType,
+            };
+
+            //Act
+            var result = await raceRepo.Update(race);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Id);
+            Assert.Contains(RaceType, result.RaceType);
+
+        }
+
+        //[Fact]
+        //public async void RaceRepo_GetAllRaces_OnSucces()
+        //{
+        //    //Arrange
+        //    context.Database.EnsureDeleted();
+        //    context.Add(MockData.MockDataRepos.GetRaceData(1));
+        //    context.Add(MockData.MockDataRepos.GetRaceData(2));
+        //    context.SaveChanges();
+
+        //    //Act
+
+        //    //Assert
+
+        //}
+
+
+        //var i = MockDataRepos.GetInventoryData(1);
+        //i.Armour = MockDataRepos.GetArmourData(1);
+        //i.Weapons = new List<Weapon>()
+        //{
+        //    MockDataRepos.GetWeaponData(1),
+        //    MockDataRepos.GetWeaponData(2),
+        //};
+
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+}
